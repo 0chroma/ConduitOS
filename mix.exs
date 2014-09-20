c36c5b9f89cff1817d01412297c6134203911937
Will Riley <will@hflw.org>
2014-09-20 06:17:12 -0400

    Updated mix boilerplate stuff to Elixir 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Conduit.Mixfile do
   def project do
     [app: :conduit,
      version: "0.0.1",
-     elixir: "~> 0.14.3",
+     elixir: "~> 1.0.0",
      deps: deps]
   end
 
@@ -12,16 +12,16 @@ defmodule Conduit.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: []]
+    [applications: [:logger]]
   end
 
-  # Dependencies can be hex.pm packages:
+  # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
   #
   # Or git/path repositories:
   #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type `mix help deps` for more examples and options
   defp deps do
